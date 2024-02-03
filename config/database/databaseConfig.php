5004bb177017d0509110dd84439b4b0bcdb749eb
Anish Dangol <dangol.anish001@gmail.com>
2024-02-03 22:28:08 +0545

    connection with database established

---
+++ config/database/databaseConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "freelancing_website";
+
+$connection = mysqli_connect($host, $user, $password, $dbname);
+
+
+
+if(!$connection){
+    echo "Connection not established";
+}else{
+    echo "Connection established";
+}
